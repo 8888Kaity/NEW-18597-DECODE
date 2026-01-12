9b24bef97c617fe458a80761ebd32244d2a439a3
Ricenoodles21 <aveionvang@gmail.com>
2026-01-12 17:00:52 -0500

    hello

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/LM4Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/LM4Auto.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode.auto;
-
+//helloooooo
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
